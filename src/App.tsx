c46308c37b0eebaa53aae23682ce626e53f490e0
Gourav Patidar <127935109+GouravPatidar91@users.noreply.github.com>
2025-11-07 22:14:55 +0530

    Replace Medicine route with ComingSoon component

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ import HealthCheckHistory from "./pages/HealthCheckHistory";
 import HealthCheckResults from "./pages/HealthCheckResults";
 import Appointments from "./pages/Appointments";
 import Medicine from "./pages/Medicine";
+import ComingSoon from "./pages/ComingSoon";
 import Profile from "./pages/Profile";
 import Settings from "./pages/Settings";
 import Emergency from "./pages/Emergency";
@@ -55,7 +56,7 @@ const App = () => (
               <Route path="/health-check-results" element={<HealthCheckResults />} />
               <Route path="/health-check-history" element={<HealthCheckHistory />} />
               <Route path="/appointments" element={<Appointments />} />
-              <Route path="/medicine" element={<Medicine />} />
+              <Route path="/medicine" element={<ComingSoon />} />
               <Route path="/vendor-dashboard" element={<VendorDashboard />} />
               <Route path="/vendor-registration" element={<VendorRegistration />} />
               <Route path="/medical-reports" element={<MedicalReports />} />
